53a7b1734f483ae27590944fd2d9061b57370662
Álvaro García <agarram0711@g.educaand.es>
2026-03-17 10:19:05 +0100

    Rutas de eliminación y comentarios

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RecetaController;
 // feed principal
 Route::get('/', [RecetaController::class, 'index']);
-
+// Crear una receta
 Route::get('/crear', [RecetaController::class, 'create'])->name('receta.create');
 Route::post('/crear', [RecetaController::class, 'store'])->name('receta.store');
-
+// Borrar una receta
+Route::delete('/receta/{id}', [RecetaController::class, 'destroy'])->name('receta.destroy');
 // Detalle de las recetas (dejar siempre al final)
 Route::get('/receta/{id}', [RecetaController::class, 'show'])->name('receta.show');
