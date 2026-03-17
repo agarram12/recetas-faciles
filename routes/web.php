f740d5ae02c327ad0f1a01f6e95cd5a23e2d416f
Álvaro García <agarram0711@g.educaand.es>
2026-03-17 09:27:10 +0100

    Rutas para la creación de recetas

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RecetaController;
-
+// feed principal
 Route::get('/', [RecetaController::class, 'index']);
 
+Route::get('/crear', [RecetaController::class, 'create'])->name('receta.create');
+Route::post('/crear', [RecetaController::class, 'store'])->name('receta.store');
+
+// Detalle de las recetas (dejar siempre al final)
 Route::get('/receta/{id}', [RecetaController::class, 'show'])->name('receta.show');
